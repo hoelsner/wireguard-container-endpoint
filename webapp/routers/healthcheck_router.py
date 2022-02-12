47255b108e4efe2ffdb0213dfd1f1d7c19396f7e
Henry Ölsner <henry@codingnetworker.com>
2022-02-12 09:25:14 +0000

    add wireguard function to healthcheck

--- webapp/routers/healthcheck_router.py
+++ webapp/routers/healthcheck_router.py
@@ -1,5 +1,9 @@
 import fastapi
+from fastapi.exceptions import HTTPException
 from routers.response_models import MessageResponseModel
+
+import utils.wireguard
+import utils.log
 
 
 healthcheck_router = fastapi.APIRouter()
@@ -17,5 +21,15 @@ async def healthcheck():
     """
     healtcheck API endpoint for the application
     """
-    # TODO: add healthchecks for application
+    logger = utils.log.LoggingUtil().logger
+
+    # verify wireguard dependency on the underlying system
+    try:
+        utils.wireguard.WgKeyUtils().generate_private_key()
+        utils.wireguard.WgKeyUtils().generate_preshared_key()
+
+    except utils.wireguard.WgKeyUtilsException as ex:
+        logger.error(f"Unable to generate keys for wireguard: {ex}")
+        raise HTTPException(status_code=500, detail="healthcheck for wireguard failed")
+
     return MessageResponseModel(message="ok")
--- webapp/tests/routers/test_routers_healtcheck.py
+++ webapp/tests/routers/test_routers_healtcheck.py
@@ -1,5 +1,10 @@
 import pytest
 from fastapi.testclient import TestClient
+import wgconfig.wgexec
+
+
+def broken_function(*args, **kwargs) -> str:
+    raise Exception("An Exception")
 
 
 class TestHealtcheckEndpoint:
@@ -12,3 +17,12 @@ class TestHealtcheckEndpoint:
         response = await test_client.get("/api/healthcheck/")
         assert response.status_code == 200
         assert response.json() == {"message": "ok"}
+
+    async def test_wireguard_failure(self, test_client: TestClient, monkeypatch):
+        """test healthcheck with failed wireguard key generation
+        """
+        with monkeypatch.context() as m:
+            m.setattr(wgconfig.wgexec, "generate_presharedkey", broken_function)
+            response = await test_client.get("/api/healthcheck/")
+            assert response.status_code == 500
+            assert response.json()["detail"] == "healthcheck for wireguard failed"
